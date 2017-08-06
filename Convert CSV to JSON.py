54cdff7a09996131625392986024fdd0cb316130
GuyZwer <guy.zwerdling@gmail.com>
2017-08-06 22:26:51 +0300

    by now the script only convers the lines of the riddle to list aka list() in python

--- Convert CSV to JSON.py
+++ Convert CSV to JSON.py
@@ -3,13 +3,15 @@
 import os
 import stat
 
-os.mknod('c1p1-conv2.csv', 0600|stat.S_IRUSR)
+os.mknod('c1p1-conv4.csv', 0600|stat.S_IRUSR)
 
+#the file name is chepter 1 part 1 and is contain 22 riddels
 original_file = open("c1p1.csv","r")
 converted_file = open("c1p1-conv.csv","w")
 #line1 = original_file.readline()
 all_list =[]
 
+#defination for find howmuch lines in the file
 def file_len(fname):
     with open(fname) as f:
         print enumerate(f)
@@ -17,6 +19,7 @@ def file_len(fname):
             pass
     return loop + 1
 
+#defination for make and order the line as a list
 def first_line_list(line1):
     word = ''
     list1 = []
@@ -32,6 +35,6 @@ for loop in range(file_len("c1p1.csv")):
     all_list.append(first_line_list(original_file.readline()))
 
 
-converted_file.write(all_list)
+print converted_file
 print all_list
 print 'this is all'
