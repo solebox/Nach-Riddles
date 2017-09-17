54c4b4a02e60f239301c79cecb87d3a9016f8e9e
guy.zwerdling <guy.zwerdling@gmail.com>
2017-09-17 14:28:45 +0300

    update some cool script evergit add *

---
+++ working/newparsing-text.py
@@ -0,0 +1,71 @@
+#this file convert the file from txt to csv
+
+# Open a file
+file_without_lines = open("file_without_lines.txt", "r",)
+middle_file = open("middle_file.txt", "w", )
+riddle_converted = open("riddle-converted.csv", "w",)
+
+def take_letter(string):
+    return string[0:2]
+
+def remove_letter(string):
+    return string[2:]
+
+
+def cut_line(string):
+    return string[:string.index("מקבילה")]
+
+def cut_torah_parasha(string):
+    return string[string.index("מקבילה"):]
+
+def cut_pitronot(string):
+    return string[len("פתרונות: "):]
+
+def remove_space(string):
+    if string[0] == ' ':
+        return string[1:]
+    else:
+        return string
+
+def while_to_remove_newline(answers):
+    while '.' not in answers:
+        answers += file_without_lines.readline().rstrip('\n')
+    return cut_pitronot(answers)
+
+
+for lop in range(2):
+    #the book name and riddles title
+    first_line = file_without_lines.readline().replace(":", "").replace(",", " ")
+    print(first_line)
+    if "ספר" in first_line:
+        bookName = first_line
+        riddlesTitle = file_without_lines.readline().replace(":", "").replace(",", " ")
+    else:
+        riddlesTitle = first_line
+    parasha = cut_line(riddlesTitle)
+    torah_parasha = cut_torah_parasha(riddlesTitle)
+    for lop in range(23):
+        riddlestring = file_without_lines.readline()
+        print ('in for lop',riddlestring)
+        if "פתרונות: " in riddlestring:
+            middle_file = open("middle_file.txt", "r")
+            answers = while_to_remove_newline(riddlestring).rstrip('\n').split(',')
+            print ('this is a list',answers)
+            for lop in answers:
+                answer = remove_space(lop)
+                print (answer)
+                allLine = middle_file.readline().rstrip('\n') + ',' + answer + '\n'
+                riddle_converted.write(allLine)
+                print(allLine)
+            middle_file = open("middle_file.txt", "w")
+            middle_file.truncate()
+
+
+        riddleNumber = take_letter(riddlestring)
+        riddle = remove_letter(riddlestring)
+        nextLineRiddle = file_without_lines.readline()
+        allRiddle = riddle.rstrip('\n') + nextLineRiddle
+        lineCSV = bookName.rstrip('\n')+ ',' + parasha + ',' + torah_parasha.rstrip('\n')+ ',' + riddleNumber+ ',' + allRiddle
+
+        middle_file.write(lineCSV)
+
--- working/parsing-txt.py
+++ working/parsing-txt.py
@@ -12,21 +12,13 @@ for riddles in range(0,3):
         for this_section in range(0,2):
             list1.append(line.rstrip())
             line = doc.readline()
-            while line =="\n":
-                line = doc.readline()
             list1.append(line.rstrip())
             for _24riddles in range(0,24):#loop for 24 riddles in this section
-                line = doc.readline()
-                while line == "\n":
                     line = doc.readline()
                 if '.' in line:#this mean that riddle is in this line
                     string_for_riddle = ''
-                    while line == "\n":
-                        line = doc.readline()
                     string_for_riddle += line.rstrip()
                     line = doc.readline()
-                    while line == "\n":
-                        line = doc.readline()
                     string_for_riddle = string_for_riddle + ' ' + line.rstrip()
                     list1.append(string_for_riddle)
                     print (list1)
