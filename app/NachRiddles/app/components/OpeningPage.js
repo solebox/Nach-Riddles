5f1f6faebffbfe6f5c474d0b9198ce90a99d62ac
guy.zwerdling <guy.zwerdling@gmail.com>
2017-09-24 16:20:51 +0300

    update the game

--- app/NachRiddles/app/components/OpeningPage.js
+++ app/NachRiddles/app/components/OpeningPage.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
-import { AppRegistry, View, Image, StyleSheet, Text } from 'react-native';
+import { AppRegistry, View, Image, StyleSheet, Text, Button } from 'react-native';
 
 export default class OpeningPage extends Component {
   render() {
     return (
-      // Try removing the `flex: 1` on the parent View.
-      // The parent will not have dimensions, so the children can't expand.
-      // What if you add `height: 300` instead of `flex: 1`?
-      <View style={{
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: 500,
-        backgroundColor: 'white'
-      }}>
-        <Text style={styles.titleText}>Nach Riddles!</Text>
-        <Image style={{height:200, width:250, borderColor: '#d6d7da', justifyContent: 'center'}} source={require("../img/NachRiddles.png")}/>
-        <Text >Nach Riddles!</Text>
+      <View style={styles.container}>
+      <View>
+        <Text style={styles.titleText}>Nach Riddles</Text>
+        <View style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          }}>
+        <Image style={{width: 250, height:200}} source={require("../img/NachRiddles.png")}/>
+        </View>
+      </View>
+      <View style={{height:80}}>
+      <Text style={styles.lineText}>ברוכים הבאים למשחק חידות הנ''ך</Text>
+      </View>
+      <View style={styles.buttonView}>
+        <Button
+          onPress={this.changeColor}
+          title="התחל משחק"
+          color={'skyblue'}
+        />
+      </View>
       </View>
     );
   }
@@ -23,12 +31,29 @@ export default class OpeningPage extends Component {
 
 
 const styles = StyleSheet.create({
+  container: {
+    height:500,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white'
+  },
   titleText: {
     color: 'skyblue',
     fontWeight: 'bold',
     fontSize: 50,
     textAlign: 'center',
   },
+  lineText: {
+    color: 'skyblue',
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  buttonView: {
+    textAlign: 'center',
+    width: 100,
+    height: 100
+  }
 
 });
 
