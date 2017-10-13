05199ff09bb8676c3bf24f802b128406ae0f9ec6
guy.zwerdling <guy.zwerdling@gmail.com>
2017-10-13 11:56:25 +0300

    update the game with the letter box

--- app/NachRiddles/app/components/common/LettersBox.js
+++ app/NachRiddles/app/components/common/LettersBox.js
@@ -5,13 +5,41 @@ import { Text, View, TouchableOpacity } from 'react-native';
 class LettersBox extends Component{
 
     render(){
-    const answer = this.props.headerText;
-    const answerlen =  answer.length;
-    console.log(answerlen)
+    //this is script for making new string of the answer and random letter
+    //the string is putting in the boxes in random way 
+    let answer = this.props.headerText;
+    console.log(answer)
+    let randomNum = Math.floor(Math.random() * 22)
     let test = [];
+    var str = "אבגדהוזחטיכלמנסעפצקרשת";
+    var res = '';
+    for (let i = 0; i < 5; i++) {
+        let randomNum = Math.floor(Math.random() * 22)
+        answer += str.charAt(randomNum)
+      }
+    console.log(answer)
+    let answerlen = answer.length;
+    console.log(answerlen)
+    let list = []
     for (let i = 0; i < answerlen; i++) {
+        let randomNum = Math.floor(Math.random() * answerlen)
+        if (list.indexOf(randomNum) >= 0) {
+          i--
+          continue
+        } else {
+          list.push(randomNum)
+        }
+      }
+    /*var res = str.split("")
+    var letter = "ת";
+    var letterPosition = res.indexOf(letter)+1;
+    ranNums.next().value;*/
+
+    for (let i = 0; i < answerlen; i++) {
+        var res = answer[list[i]]
         test.push(
             <TouchableOpacity style={styles.boxStyle} key={i}>
+              <Text>{res}</Text>
             </TouchableOpacity>
         );
       }
@@ -24,9 +52,10 @@ class LettersBox extends Component{
 const styles = {
   viewStyle:{
     flexDirection: 'row',
+    flexWrap: 'wrap',
     alignItems:'center',
     justifyContent:'center',
-    height: 80,
+    height: 130,
     borderWidth: 2,
     position: 'relative'
   },
