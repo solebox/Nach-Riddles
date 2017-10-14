8ffc69bec2f797e2723e51878eec8cd7bcae9c61
guy.zwerdling <guy.zwerdling@gmail.com>
2017-10-15 00:05:16 +0300

    update again

--- app/NachRiddles/app/components/HomeScreen.js
+++ app/NachRiddles/app/components/HomeScreen.js
@@ -69,68 +69,6 @@ export default class HomeScreen extends React.Component {
   }
 }
 
-
-
-      /*
-      <View style={styles.container}>
-        <View>
-          <Image
-            style={{ flex:1, resizeMode: 'stretch'}}
-            source={require('../img/riddle.jpg')}
-          />
-          <Text style={styles.titleText}>אביעה חידות מני קדם</Text>
-          <View style={styles.imageViewer}>
-            <View>
-              <Image
-                style={{ width: 360, height: 165 }}
-                source={require('../img/Book.png')}
-              />
-            </View>
-          </View>
-        </View>
-        <View style={{ height: 50 }}>
-          <Text style={styles.lineText}>ברוכים הבאים למשחק חידות הנ''ך</Text>
-        </View>
-
-        <TouchableOpacity
-          onPress={() => this.props.navigation.navigate('BookSelector')}
-        >
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>התחל</Text>
-          </View>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          onPress={() => this.props.navigation.navigate('Levels')}
-        >
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>שלבים</Text>
-          </View>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          onPress={() => this.props.navigation.navigate('Settings')}
-        >
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>הגדרות</Text>
-          </View>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          onPress={() => this.props.navigation.navigate('About')}
-        >
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>אודות</Text>
-          </View>
-        </TouchableOpacity>
-      </View>
-    )
-  }
-}
-
-//export HomeScreen;
-*/
-
 const styles = StyleSheet.create({
   container: {
     flex:1,
@@ -172,9 +110,6 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 10,
     borderColor: 'white',
-
-
-
   },
   buttonText: {
     fontSize: 30,
@@ -183,44 +118,6 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     backgroundColor: 'rgba(0,0,0,0)'
   },
-
-/*
-  titleText: {
-    color: '#2196F3',
-    fontWeight: 'bold',
-    fontSize: 50,
-    textAlign: 'center'
-  },
-  imageViewer: {
-    height: 160,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  lineText: {
-    color: '#2196F3',
-    fontWeight: 'bold',
-    fontSize: 20,
-    textAlign: 'center'
-  },
-  buttonView: {
-    textAlign: 'center',
-    width: 170,
-    height: 45
-  },
-  button: {
-    marginBottom: 5,
-    width: 200,
-    height: 45,
-    alignItems: 'center',
-    backgroundColor: '#2196F3'
-  },
-  buttonText: {
-    fontSize: 30,
-    padding: 1,
-    color: 'black',
-    fontWeight: 'bold',
-    backgroundColor: 'rgba(0,0,0,0)'
-  }*/
 })
 
 // skip this line if using Create React Native App
