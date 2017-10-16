e8dfa67279f6c45a304681c05e33f5a3f7f33b79
guy.zwerdling <guy.zwerdling@gmail.com>
2017-10-16 19:59:26 +0300

    for now there is a nice view in the Riddles section, stile the button dosnt work

---
+++ app/NachRiddles/app/components/common/RiddleSectionsHeader1.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import { Text, View } from 'react-native';
+
+
+class RiddleSectionsHeader1 extends Component{
+  render(){
+    return(
+      <View style={styles.viewStyle}>
+        <Text style={styles.textStyle}>{this.props.headerText}</Text>
+      </View>
+    )
+  }
+};
+
+const styles = {
+  viewStyle:{
+    flex:1,
+    alignItems:'center',
+    backgroundColor: '#472747',
+    borderColor: 'black',
+    borderWidth: 2,
+    height: 60,
+    margin:0,
+    flexWrap: 'wrap'
+  },
+  textStyle: {
+    textAlign: 'center',
+    fontSize:15,
+    color: 'white',
+  }
+
+};
+
+export {RiddleSectionsHeader1};
